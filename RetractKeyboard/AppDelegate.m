922607fd6fea5911c7fbdc5a3a91bc1f267e1de6
NFJ <NFJ@dmc.local>
2013-01-30 12:27:40 +0800

    contact

--- RetractKeyboard/AppDelegate.m
+++ RetractKeyboard/AppDelegate.m
@@ -4,6 +4,8 @@
 //
 //  Created by NFJ on 13-1-30.
 //  Copyright (c) 2013年 NiuFJ. All rights reserved.
+//  https://github.com/niufj/RetractKeyboard
+//  QQ:356735966
 //
 
 #import "AppDelegate.h"
